10dc8ef46c0dbc4c5acbfe51df5a1812e59e2bc9
Yeferson Correa Cardona <ycorreca@SUMMIT.COM.CO>
2024-01-29 11:49:39 -0500

    Se agregran unos ejemplos

---
+++ Algoritmos/Algoritmos.EstructuraDeDatos/Arrays/ReverseArray/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos.EstructuraDeDatos.Arrays.ReverseArray.Ejercicios
+{
+    public class Ejercicio1
+    {
+        public string reverseWords(string s)
+        {
+            string[] cadena = s.Split('.');
+
+            Array.Reverse(cadena);
+            string cadenaInvertida = String.Join(".", cadena);
+
+            return cadenaInvertida;
+        }
+    }
+}
--- Algoritmos/Algoritmos/Program.cs
+++ Algoritmos/Algoritmos/Program.cs
@@ -1,7 +1,12 @@
 ﻿using Algoritmos.EstructuraDeDatos.Arrays.ReverseArray;
+using Algoritmos.EstructuraDeDatos.Arrays.ReverseArray.Ejercicios;
 
-Explicacion explicacion = new Explicacion();
-int[] values = explicacion.ReverseArrayExtraArray(new int[] {1,2,3,4,5,6,7,8,9});
-int[] values2 = explicacion.ReverseArray(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0, 8);
-Console.WriteLine($"Resultado 1{values[0]}");
-Console.WriteLine($"Resultado 2{values2[0]}");
+//Explicacion explicacion = new Explicacion();
+//int[] values = explicacion.ReverseArrayExtraArray(new int[] {1,2,3,4,5,6,7,8,9});
+//int[] values2 = explicacion.ReverseArray(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0, 8);
+//Console.WriteLine($"Resultado 1{values[0]}");
+//Console.WriteLine($"Resultado 2{values2[0]}");
+
+Ejercicio1 ejercicio1 = new Ejercicio1();
+string value = ejercicio1.reverseWords("hello.hello");
+Console.WriteLine(value.ToString());
